24eb3eed3439d4c33a5baf2b83e6b141ea4d7f99
carajillu <joan.clark88@gmail.com>
2024-10-03 18:20:42 +0100

    seelct frames first commit

--- select_frames.py
+++ select_frames.py
@@ -1,9 +1,8 @@
 import argparse
-
+import numpy as np
+from ase.io import read, write
 def parse_args():
     parser=argparse.ArgumentParser()
-    parser.add_argument("--initial_state",nargs="?",help="Path to the optimized structure of the initial state",default="initial.xyz")
-    parser.add_argument("--final_state",nargs="?",help="Path to the optimized structure of the final state",default="final.xyz")
     parser.add_argument("--fwd_trj",nargs="?",help="Path to the forward trajectory",default="fwd.xyz")
     parser.add_argument("--bwd_trj",nargs="?",help="Path to the backward trajectory",default="bwd.xyz")
     parser.add_argument("--atom_pairs",nargs="+",help="Atom pairs to consider for the selection of frames (example: --atom_pairs \"0 1\" \"2 3\")",default=[[0,1],[2,3]])
@@ -14,13 +13,33 @@ def parse_args():
 def get_atom_pairs(args):
     atom_pairs=[]
     for pair in args.atom_pairs:
-        pair=pair.split()
-        pair=[int(i) for i in pair]
+        if isinstance(pair,str):
+            pair=pair.split()
+            pair=[int(i) for i in pair]
         atom_pairs.append(pair)
     return atom_pairs
 
+
+
+def make_histogram(distance_matrix,spacing,extend):
+    '''
+    returns a histogram object of the distance matrix, with the number of bins defined by the spacing and the minimum
+    and maximum values in each dimension, which are extended by the extend parameter.
+    '''
+    nbins=int((max(distance_matrix)+extend)-(min(distance_matrix)-extend)/spacing)
+    hist=np.histogramdd(distance_matrix,bins=spacing,range=((min(distance_matrix)-extend,max(distance_matrix)+extend),)*len(distance_matrix))
+    return hist
+
+def get_distance_matrix(atoms,atom_pairs):
+    distance_matrix=[]
+    for pair in atom_pairs:
+        distance_matrix.append(atoms.get_distance(pair[0],pair[1]))
+    return distance_matrix
 
 if __name__=="__main__":
     args=parse_args()
     atom_pairs=get_atom_pairs(args)
     print(atom_pairs)
+    
+    
+
